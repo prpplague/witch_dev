2de235384da6232c2f96f5e6c3a56d3e63526e10
David Anders <danders.dev@gmail.com>
2014-01-22 17:36:55 -0600

    update pointers for function call

--- emulation-tests/logic-tests.c
+++ emulation-tests/logic-tests.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
-int add(int reg_a, int reg_b) {
+int add(int *reg_a, int *reg_b) {
 
     int pulse=0;
     int setflag=0;
 
-	for (pulse=0; pulse < 10; pulse++) {
-		printf("Pulse Count = %d : ",pulse);
-		reg_a++;
-		if (reg_a == 10) {
-		    setflag = 1;
-		    reg_a = 0;
-		}
+    for (pulse=0; pulse < 10; pulse++) {
+	printf("Pulse Count = %d : ",pulse);
+	(*reg_a)++;
+	if (*reg_a == 10) {
+	    setflag = 1;
+	    *reg_a = 0;
+	}
 
-		if (setflag)
-		    reg_b++;
+	if (setflag)
+	    (*reg_b)++;
 
-		printf("reg_a = %d : reg_b = %d\n",reg_a,reg_b);
+	printf("reg_a = %d : reg_b = %d\n",*reg_a,*reg_b);
 
 	}
 
@@ -26,12 +26,13 @@ int add(int reg_a, int reg_b) {
 
 void main (void) {
 
-int tape_01=4;
-int reg_10=0;
-int reg_20=0;
+    int tape_01=4;
+    int reg_10=0;
+    int reg_20=0;
 
 
-add (tape_01,reg_10);
+    add (&tape_01,&reg_10);
 
+    printf("tape_01 = %d : reg_10 = %d\n",tape_01,reg_10);
 
 }
